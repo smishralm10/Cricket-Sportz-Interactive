5e250f0e3ed24757ee5fc3ba061160fb1cc60527
Shreyansh Mishra <smishralm10@gmail.com>
2023-12-19 11:00:14 +0530

    Initial Commit

---
+++ Cricket/CricketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricketApp.swift
+//  Cricket
+//
+//  Created by Shreyansh Mishra on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CricketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
