54b4d289dfb2a41af62b7b7195cb75ab26432ca4
Shreyansh Mishra <smishralm10@gmail.com>
2023-12-21 10:06:35 +0530

    UI for home screen

--- Cricket/UI/Screens/ContentView.swift
+++ Cricket/UI/Screens/ContentView.swift
@@ -16,13 +16,9 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Home()
         }
-        .padding()
         .environment(\.injected, container)
     }
 }
